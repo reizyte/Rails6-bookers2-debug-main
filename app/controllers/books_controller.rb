c8a05d0bbba89014107b3780bdb1466e09d8275e
reizyte <112692614+reizyte@users.noreply.github.com>
2022-10-09 18:31:49 +0000

    comment-iine

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,13 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
     @books = Book.all
     @book = Book.new
+    
   end
 
   def create
