b46dfe906d876a69d311649117b6cdb18110202e
reizyte <112692614+reizyte@users.noreply.github.com>
2022-10-11 08:55:54 +0000

    7c

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorited_books, through: :favorites, source: :book
+  has_many :group_users
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
  end
   resources :users, only: [:index,:show,:edit,:update] do
    resource :relationships, only: [:create, :destroy]
+   resources :groups,only: [:index, :show, :edit, :create, :update, :new, :show]
    get 'followings' =>'relationships#followings', as: 'followings'
    get 'followers' =>'relationships#followers', as: 'followers'
   end
