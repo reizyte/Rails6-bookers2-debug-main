4c02117c6de9b8731cf544b4a747bda36aa5b6ab
reizyte <112692614+reizyte@users.noreply.github.com>
2022-10-11 06:25:57 +0000

    7a

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,14 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
+import raty from 'raty-js'
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.$ = window.jQuery = require('jquery');
