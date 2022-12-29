76b1f14b3b0f8e36cea9c9ebf2d01cb977085598
reizyte <112692614+reizyte@users.noreply.github.com>
2022-12-29 05:48:03 +0000

    インデント修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,15 +9,18 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    #@books = Book.all.order(params[:sort])
+    @books = Book.order("RANDOM()").all
+
+    #@books = Book.all
     @book = Book.new
-    to  = Time.current.at_end_of_day
-    from  = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-      sort {|a,b|
-        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
-        a.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }
+    # to  = Time.current.at_end_of_day
+    # from  = (to - 6.day).at_beginning_of_day
+    # @books = Book.includes(:favorited_users).
+    #   sort {|a,b|
+    #     b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+    #     a.favorited_users.includes(:favorites).where(created_at: from...to).size
+    #   }
 
   end
 
